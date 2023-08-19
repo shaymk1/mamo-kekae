04a2567909f9f126a3d262bffda3b34949e4092e
unknown <mkekae@gmail.com>
2023-08-19 12:27:58 +0200

    deleted mix-it-up library

---
+++ script.js
@@ -0,0 +1,12 @@
+//********** ***// mix-it-up //********** ***//
+// AMD
+
+// var mixitup = require("mixitup");
+// const containerEl = document.querySelector(".projects__container");
+// var mixer = mixitup(containerEl, {
+// 	animation: {
+// 		enable: false,
+// 	},
+// });
+// mixer.filter("*");
+
